b523e512407419d7fb585ba13702b17ee6790527
turret <turret@turret-studios.tk>
2020-12-12 22:34:06 -0600

    add reload

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -17,7 +17,7 @@ export default class pull extends Command {
             embed: {
                 color: config.colors.success,
                 title: "Pulled!",
-                description: `Please restart the bot (\`${config.prefix}restart\`) to apply changes outside of the commands and events folder. It may take a little bit time to complete the pull and compile.`,
+                description: `Please reload the bot (\`${config.prefix}reload\`) to apply command changes, restart the bot (\`${config.prefix}restart\`) to apply bot changes. It may take a little bit time to complete the pull and compile.`,
             },
         });
     };
---
+++ src/commands/reload.ts
@@ -0,0 +1,56 @@
+import Command from "../classes/abstract/command";
+import util from "../types/util";
+import * as Eris from "eris";
+import * as fs from "fs";
+import * as resources from "../resources";
+import * as config from "../config/config";
+
+export default class reload extends Command {
+    public execute = async (
+        util: util,
+        command: string,
+        args: string[],
+        msg: Eris.Message,
+    ) => {
+        util.cmds.clear();
+        // re-register all commands
+        const cmdFiles = await fs.readdirSync(`${__dirname}/commands`);
+
+        cmdFiles.forEach(async (name) => {
+            if (name.endsWith(".js")) {
+                const cmd = name.replace(".js", "");
+                const cmdFile = await resources.reload(
+                    `${__dirname}/commands/${name}`,
+                );
+
+                try {
+                    if (cmdFile !== null)
+                        if (cmdFile.default !== null) {
+                            const cmdClass = new cmdFile.default();
+                            util.cmds.set(cmd, cmdClass);
+                        }
+                } catch (err) {
+                    console.warn(`Failed to register command file ${name}`);
+                }
+            }
+        });
+
+        const mesg = await util.client.createMessage(msg.channel.id, {
+            embed: {
+                color: config.colors.success,
+                title: "Reloaded!",
+                description: `The bot has re-registered it's commands!`,
+            },
+        });
+    };
+
+    constructor() {
+        super(
+            "reload",
+            "Developer",
+            "Developer",
+            "",
+            "re-registers all bot commands",
+        );
+    }
+}
